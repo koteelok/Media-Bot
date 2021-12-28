845bba88187b2b7d5116e64fdf8c2359d7bb9ab5
koteelok <musaasukhanov@gmail.com>
2021-12-28 22:48:30 +0300

    Update pause.js

--- src/commands/pause.js
+++ src/commands/pause.js
@@ -13,7 +13,6 @@ module.exports = class extends SlashCommand {
         const { client } = require('..');
         const guild = client.guilds.cache.get(ctx.guildID);
         const member = guild.members.cache.get(ctx.member.id);
-
         const queue = client.player.getQueue(guild.id);
 
         if (!queue) return ctx.sendFollowUp({ content: `No music currently playing ${member}... try again ? ❌` });
